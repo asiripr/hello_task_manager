58d849a0d262eb1001b540f0e9ca83f886097c28
asiripr <asiripramodaya2000@gmail.com>
2024-10-12 19:13:26 +0530

    coded the provider

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:hello_task_manager/models/task_model.dart';
 
 class TaskProvider with ChangeNotifier{
-  
+  List<Task> _tasks = [];
+
+  List<Task> get tasks => _tasks; // no idea
+
+  void addTask(Task task){
+    _tasks.add(task);
+    notifyListeners(); // no idea
+  }
+
+  void toggleTaskStatus(int index){
+    _tasks[index].isCompleted = !_tasks[index].isCompleted;
+    notifyListeners();
+  }
+
+  void removeTask(int index){
+    _tasks.removeAt(index);
+    notifyListeners();
+  }
+
+  // no idea
+
+  List<Task> get pendingTasks =>
+      _tasks.where((task) => !task.isCompleted).toList();
+
+  List<Task> get completedTasks =>
+      _tasks.where((task) => task.isCompleted).toList();
+
 }
